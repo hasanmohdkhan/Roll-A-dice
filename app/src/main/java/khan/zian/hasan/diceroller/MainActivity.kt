fa3326bbc479efec28daaa795309235ca3fdccc2
club5indie <club5indie@gmail.com>
2019-06-11 12:18:23 +0530

    feat: Added sound on every rolling

--- app/src/main/java/khan/zian/hasan/diceroller/MainActivity.kt
+++ app/src/main/java/khan/zian/hasan/diceroller/MainActivity.kt
@@ -1,5 +1,6 @@
 package khan.zian.hasan.diceroller
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
@@ -7,6 +8,7 @@ import android.widget.ImageView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private var mediaPlayer: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +23,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun diceRoll() {
+        mediaPlayer = MediaPlayer.create(this, R.raw.dice_roll_sound)
+
+
         val randomInt = Random().nextInt(6) + 1
         val result: ImageView = findViewById(R.id.img_roll_dice_number)
         val drawableResource = when (randomInt) {
@@ -31,6 +36,8 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
+
+        mediaPlayer?.start()
         result.setImageResource(drawableResource)
 
     }
